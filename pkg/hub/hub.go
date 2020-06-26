3d36534970e752bf12790d083d53b97ff7acedec
Cesar Del Solar <delsolar@gmail.com>
2020-06-26 19:12:08 -0400

    delete a lot of comments mostly

--- pkg/hub/hub.go
+++ pkg/hub/hub.go
@@ -38,21 +38,12 @@ type Hub struct {
 	unregister chan *Client
 
 	pubsub *PubSub
-	// subconn *nats.Conn
-	// subchan chan *nats.Msg
-	// Topics (i.e. realms)
-
-	// gameStore       gameplay.GameStore
-	// soughtGameStore gameplay.SoughtGameStore
-	// config          *config.Config
 
 	realmMutex sync.Mutex
 	// Each realm has a list of clients in it.
 	realms map[Realm]map[*Client]bool
 
 	broadcastRealm chan RealmMessage
-
-	// eventChan chan *entity.EventWrapper
 }
 
 func NewHub(cfg *config.Config) (*Hub, error) {
@@ -70,15 +61,6 @@ func NewHub(cfg *config.Config) (*Hub, error) {
 		clientsByUserID: make(map[string]map[*Client]bool),
 		realms:          make(map[Realm]map[*Client]bool),
 		pubsub:          pubsub,
-		// subconn:         subconn,
-		// subchan:         make(chan *nats.Msg, 64),
-		// pubconn:           pub,
-		// eventChan should be buffered to keep the game logic itself
-		// as fast as possible.
-		// eventChan:       make(chan *entity.EventWrapper, 50),
-		// gameStore:       gameStore,
-		// soughtGameStore: soughtGameStore,
-		// config:          cfg,
 	}, nil
 }
 
@@ -166,14 +148,6 @@ func (h *Hub) Run() {
 			} else {
 				log.Error().Msg("unregistered-but-not-in-map")
 			}
-		// case message := <-h.broadcast:
-		// 	for client := range h.clients {
-		// 		select {
-		// 		case client.send <- message:
-		// 		default:
-		// 			h.removeClient(client)
-		// 		}
-		// 	}
 
 		case message := <-h.broadcastRealm:
 			log.Debug().Str("realm", string(message.realm)).
@@ -189,45 +163,6 @@ func (h *Hub) Run() {
 		}
 	}
 }
-
-// RunGameEventHandler runs a separate loop that just handles game events,
-// and forwards them to the appropriate sockets. All other events, like chat,
-// should be handled in the Run function (I think, subject to change).
-// func (h *Hub) RunGameEventHandler() {
-// 	for {
-// 		select {
-// 		case w := <-h.eventChan:
-// 			realm := Realm(w.GameID())
-// 			err := h.sendToRealm(realm, w)
-// 			if err != nil {
-// 				log.Err(err).Str("realm", string(realm)).Msg("sending to realm")
-// 			}
-// 			n := len(h.eventChan)
-// 			// Also send any backed up events to the appropriate realms.
-// 			if n > 0 {
-// 				log.Info().Int("backpressure", n).Msg("game event channel")
-// 			}
-// 			for i := 0; i < n; i++ {
-// 				w := <-h.eventChan
-// 				err := h.sendToRealm(realm, w)
-// 				if err != nil {
-// 					log.Err(err).Str("realm", string(realm)).Msg("sending to realm")
-// 				}
-// 			}
-
-// 		}
-// 	}
-// }
-
-// since addNewRealm can be called from different goroutines, we must protect
-// the realm map.
-// func (h *Hub) addNewRealm(id string) Realm {
-// 	h.realmMutex.Lock()
-// 	defer h.realmMutex.Unlock()
-// 	realm := Realm(id)
-// 	h.realms[realm] = make(map[*Client]bool)
-// 	return realm
-// }
 
 // since addToRealm can be called multithreaded, we must protect the map with
 // a mutex.
@@ -282,97 +217,3 @@ func (h *Hub) removeFromRealm(client *Client) {
 	}
 	h.clients[client] = NullRealm
 }
-
-// func (h *Hub) deleteRealm(realm Realm) {
-// 	// XXX: maybe disallow if users in realm.
-// 	h.realmMutex.Lock()
-// 	defer h.realmMutex.Unlock()
-// 	delete(h.realms, realm)
-// }
-
-// func (h *Hub) broadcastEvent(w *entity.EventWrapper) error {
-// 	bts, err := w.Serialize()
-// 	if err != nil {
-// 		return err
-// 	}
-// 	h.broadcast <- bts
-// 	return nil
-// }
-
-// func (h *Hub) NewSeekRequest(cs *pb.SeekRequest) error {
-// 	evt := entity.WrapEvent(cs, pb.MessageType_SEEK_REQUEST, "")
-// 	return h.broadcastEvent(evt)
-// }
-
-// func (h *Hub) DeleteSeek(id string) error {
-// 	// essentially just send the same game accepted event back.
-// 	evt := entity.WrapEvent(&pb.GameAcceptedEvent{RequestId: id}, pb.MessageType_GAME_ACCEPTED_EVENT, "")
-// 	return h.broadcastEvent(evt)
-// }
-
-// func (h *Hub) sendToClient(username string, evt *entity.EventWrapper) error {
-// 	clients := h.clientsByUsername[username]
-// 	if clients == nil {
-// 		return errors.New("client not in list")
-// 	}
-// 	bts, err := evt.Serialize()
-// 	if err != nil {
-// 		return err
-// 	}
-// 	for _, client := range clients {
-// 		client.send <- bts
-// 	}
-// 	return nil
-// }
-
-// func (h *Hub) sendRealmData(ctx context.Context, realm Realm, username string) {
-// 	// Send the client relevant data depending on its realm(s). This
-// 	// should be called when the client joins a realm.
-// 	clients := h.clientsByUsername[username]
-// 	for _, client := range clients {
-// 		if realm == Realm(LobbyRealm) {
-// 			msg, err := h.openSeeks(ctx)
-// 			if err != nil {
-// 				log.Err(err).Msg("getting open seeks")
-// 				return
-// 			}
-// 			client.send <- msg
-// 		} else {
-// 			// assume it's a game
-// 			// c.send <- c.hub.gameRefresher(realm)
-// 			msg, err := h.gameRefresher(ctx, realm, username)
-// 			if err != nil {
-// 				log.Err(err).Msg("getting game info")
-// 				return
-// 			}
-// 			client.send <- msg
-// 		}
-// 	}
-
-// }
-
-// // Not sure where else to put this
-// func (h *Hub) openSeeks(ctx context.Context) ([]byte, error) {
-// 	sgs, err := h.soughtGameStore.ListOpen(ctx)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-
-// 	pbobj := &pb.SeekRequests{Requests: []*pb.SeekRequest{}}
-// 	for _, sg := range sgs {
-// 		pbobj.Requests = append(pbobj.Requests, sg.SeekRequest)
-// 	}
-// 	evt := entity.WrapEvent(pbobj, pb.MessageType_SEEK_REQUESTS, "")
-// 	return evt.Serialize()
-// }
-
-// func (h *Hub) gameRefresher(ctx context.Context, realm Realm, username string) ([]byte, error) {
-// 	// Assume the realm is a game ID. We can expand this later.
-// 	entGame, err := h.gameStore.Get(ctx, string(realm))
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	evt := entity.WrapEvent(entGame.HistoryRefresherEvent(),
-// 		pb.MessageType_GAME_HISTORY_REFRESHER, entGame.GameID())
-// 	return evt.Serialize()
-// }
--- pkg/hub/parser.go
+++ pkg/hub/parser.go
@@ -69,88 +69,6 @@ func (h *Hub) parseAndExecuteMessage(ctx context.Context, msg []byte, c *Client)
 			return err
 		}
 
-		/* client gameplay event
-		evt, ok := ew.Event.(*pb.ClientGameplayEvent)
-		if !ok {
-			// This really shouldn't happen
-			return errors.New("cge unexpected typing error")
-		}
-		err := gameplay.PlayMove(ctx, h.gameStore, sender, evt)
-		if err != nil {
-			return err
-		}
-		*/
-
-		// seek request accepted:
-	// 	sg, err := h.soughtGameStore.Get(ctx, evt.RequestId)
-	// 	if err != nil {
-	// 		return err
-	// 	}
-	// 	requester := sg.SeekRequest.User.Username
-	// 	if requester == sender {
-	// 		log.Info().Str("sender", sender).Msg("canceling seek")
-	// 		err := gameplay.CancelSoughtGame(ctx, h.soughtGameStore, evt.RequestId)
-	// 		if err != nil {
-	// 			return err
-	// 		}
-	// 		// broadcast a seek deletion.
-	// 		err = h.DeleteSeek(evt.RequestId)
-	// 		if err != nil {
-	// 			return err
-	// 		}
-	// 		return err
-	// 	}
-
-	// 	players := []*macondopb.PlayerInfo{
-	// 		{Nickname: sender, RealName: sender},
-	// 		{Nickname: requester, RealName: requester},
-	// 	}
-	// 	log.Debug().Interface("seekreq", sg.SeekRequest).Msg("seek-request-accepted")
-
-	// 	g, err := gameplay.InstantiateNewGame(ctx, h.gameStore, h.config,
-	// 		players, sg.SeekRequest.GameRequest)
-	// 	if err != nil {
-	// 		return err
-	// 	}
-	// 	// Broadcast a seek delete event, and send both parties a game redirect.
-	// 	h.soughtGameStore.Delete(ctx, evt.RequestId)
-	// 	err = h.DeleteSeek(evt.RequestId)
-	// 	if err != nil {
-	// 		return err
-	// 	}
-	// 	// This event will result in a redirect.
-	// 	ngevt := entity.WrapEvent(&pb.NewGameEvent{
-	// 		GameId: g.GameID(),
-	// 	}, pb.MessageType_NEW_GAME_EVENT, "")
-
-	// 	h.sendToClient(sender, ngevt)
-	// 	h.sendToClient(requester, ngevt)
-	// 	// Create a new realm to put these players in.
-	// 	realm := h.addNewRealm(g.GameID())
-	// 	h.addToRealm(realm, sender)
-	// 	h.addToRealm(realm, requester)
-	// 	log.Info().Str("newgameid", g.History().Uid).
-	// 		Str("sender", sender).
-	// 		Str("requester", requester).
-	// 		Str("onturn", g.NickOnTurn()).Msg("game-accepted")
-
-	// 	// Now, reset the timer and register the event change hook.
-	// 	err = gameplay.StartGame(ctx, h.gameStore, h.eventChan, g.GameID())
-	// 	if err != nil {
-	// 		return err
-	// 	}
-
-	// case pb.MessageType_CLIENT_GAMEPLAY_EVENT:
-	// 	evt, ok := ew.Event.(*pb.ClientGameplayEvent)
-	// 	if !ok {
-	// 		// This really shouldn't happen
-	// 		return errors.New("cge unexpected typing error")
-	// 	}
-	// 	err := gameplay.PlayMove(ctx, h.gameStore, sender, evt)
-	// 	if err != nil {
-	// 		return err
-	// 	}
-
 	case pb.MessageType_JOIN_PATH:
 		ew, err := entity.EventFromByteArray(msg)
 		if err != nil {
@@ -166,20 +84,14 @@ func (h *Hub) parseAndExecuteMessage(ctx context.Context, msg []byte, c *Client)
 	case pb.MessageType_UNJOIN_REALM:
 		h.removeFromRealm(c)
 
-	// case pb.MessageType_TIMED_OUT:
-	// 	evt, ok := ew.Event.(*pb.TimedOut)
-	// 	if !ok {
-	// 		return errors.New("to unexpected typing error")
-	// 	}
-	// 	// Verify the timeout.
-	// 	err := gameplay.TimedOut(ctx, h.gameStore, sender, evt.Username, evt.GameId)
-	// 	if err != nil {
-	// 		return err
-	// 	}
+	case pb.MessageType_TIMED_OUT:
+		err := h.pubsub.natsconn.Publish(extendTopic(c, "ipc.pb.timedOut"), msg[1:])
+		if err != nil {
+			return err
+		}
 
 	default:
 		return fmt.Errorf("message type %v not yet handled", msg[0])
-
 	}
 
 	return nil
